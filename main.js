b0e4d78c44582fc81652f892b4be269d3c7e3b13
bulbulkae <bqassymbek@gmail.com>
2026-09-09 16:23:13 +0500

    2nd

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 window.alert("Hello, world!");
 console.log("Hello from my PC!");
+console.log("You should have a perfect knowledge about your own and unperfect knowledge about others. Learning something is practice a lot of practice and practice. Not comring you with someone.");
